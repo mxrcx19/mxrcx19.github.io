b21c5f54a89fc5b4766ef5bd2513bcb2535cb8bc
mxrcx19 <64331649+mxrcx19@users.noreply.github.com>
2024-12-09 15:20:42 +0100

    test

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const ctx = canvas.getContext('2d');
 startButton.addEventListener('click', () => {
     startScreen.classList.remove('visible');
     gameScreen.classList.add('visible');
+    permission();
     gameLoop();
 });
 
@@ -195,20 +196,22 @@ if(typeof DeviceMotionEvent !== "undefined") {
     alert("Dein Gerät unterstützt die Bewegungssensor-API nicht.");
 }
 */
-if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
-    // (optional) Do something before API request prompt.
-    DeviceMotionEvent.requestPermission()
-        .then( response => {
-        // (optional) Do something after API prompt dismissed.
-        if ( response == "granted" ) {
-            window.addEventListener( "devicemotion", (e) => {
-                // do something for 'e' here.
-            })
-        }
-    })
-        .catch( console.error )
-} else {
-    alert( "DeviceMotionEvent is not defined" );
+function permission () {
+    if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
+        // (optional) Do something before API request prompt.
+        DeviceMotionEvent.requestPermission()
+            .then( response => {
+            // (optional) Do something after API prompt dismissed.
+            if ( response == "granted" ) {
+                window.addEventListener( "devicemotion", (e) => {
+                    // do something for 'e' here.
+                })
+            }
+        })
+            .catch( console.error )
+    } else {
+        alert( "DeviceMotionEvent is not defined" );
+    }
 }
 
 let vx = 0, vy = 0;
